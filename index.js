5c4346597c22ee7eb2d30a9434c59841dfff96ea
DawkinsJonathanOchieng <dawkinsjonathan871@gmail.com>
2022-08-25 13:00:58 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(ary, str){
+    return ary.filter((element) => element.toUpperCase() === str.toUpperCase())
+}
+function fuzzyMatch(ary, str){
+    return ary.filter((element) => element.slice(0, str.length) === str)
+}
+function matchName(ary, str){
+    return ary.filter((element) => element["name"] === str)
+}
